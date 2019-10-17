afaedb34aac1e76df21df2a06e2e3d19a38e9c3c
art160630 <art160630@utdallas.edu>
2019-10-17 11:46:18 -0500

    created DESEncryptor class paralleling that of the AESEncryptor class, untested because intelliJ is acting strange on my end and won't build/run

--- src/com/cyberchase/cyberchaser/Main.java
+++ src/com/cyberchase/cyberchaser/Main.java
@@ -1,6 +1,7 @@
 package com.cyberchase.cyberchaser;
 
 import com.cyberchase.cyberchaser.encryption.AESEncryptor;
+import com.cyberchase.cyberchaser.encryption.DESEncryptor;
 
 import java.lang.String;
 
@@ -12,6 +13,14 @@ public class Main {
         String originalString = "howtodoinjava.com";
         String encryptedString = AESEncryptor.encryptFile(originalString, secretKey);
         String decryptedString = AESEncryptor.decryptFile(encryptedString, secretKey);
+
+        System.out.println(originalString);
+        System.out.println(encryptedString);
+        System.out.println(decryptedString);
+
+        originalString = "newteststring";
+        encryptedString = DESEncryptor.encryptFile(originalString, secretKey);
+        decryptedString = DESEncryptor.decryptFile(encryptedString, secretKey);
 
         System.out.println(originalString);
         System.out.println(encryptedString);
