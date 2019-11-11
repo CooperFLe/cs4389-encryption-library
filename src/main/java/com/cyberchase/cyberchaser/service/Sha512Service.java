63b28356d915ef88f4936bb5ed0e3cc7593b9d39
Cooper F. Le <cooperfle@utdallas.edu>
2019-11-11 16:03:54 -0600

    refactor: rename service

--- src/main/java/com/cyberchase/cyberchaser/service/Sha512Service.java
+++ src/main/java/com/cyberchase/cyberchaser/service/Sha512Service.java
@@ -4,7 +4,7 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
-public class Sha512 {
+public class Sha512Service {
 
     public static String hash(String value) {
         try {
