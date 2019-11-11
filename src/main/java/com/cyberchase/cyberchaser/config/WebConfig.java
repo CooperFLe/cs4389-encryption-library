fc716ba7c08acabdfe4d3b4ba3b492b244ff5a8e
Cooper F. Le <cooperfle@utdallas.edu>
2019-11-11 17:11:29 -0600

    fix: redirect root to swagger

---
+++ src/main/java/com/cyberchase/cyberchaser/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.cyberchase.cyberchaser.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "/swagger-ui.html");
+    }
+
+}
