8258cf76d4a61e2897a2ce66964e0ebff982f544
Cooper F. Le <cooperfle@utdallas.edu>
2019-11-11 16:04:02 -0600

    feat: add rsa endpoints

--- src/main/java/com/cyberchase/cyberchaser/controller/EncryptionController.java
+++ src/main/java/com/cyberchase/cyberchaser/controller/EncryptionController.java
@@ -2,31 +2,55 @@ package com.cyberchase.cyberchaser.controller;
 
 import com.cyberchase.cyberchaser.service.AESService;
 import com.cyberchase.cyberchaser.service.DESService;
+import com.cyberchase.cyberchaser.service.RSAService;
+import com.cyberchase.cyberchaser.model.RSAResult;
+import com.cyberchase.cyberchaser.model.RSAPayload;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EncryptionController {
 
-    @RequestMapping("/aes/enc/{key}/{value}")
+    @RequestMapping(value = "/aes/enc/{key}/{value}", method = RequestMethod.POST)
     public String aesEncryption(@PathVariable("key") String key, @PathVariable("value") String value) {
         return AESService.encryptFile(value, key);
     }
 
-    @RequestMapping("/aes/dec/{key}/{value}")
+    @RequestMapping(value = "/aes/dec/{key}/{value}", method = RequestMethod.POST)
     public String aesDecryption(@PathVariable("key") String key, @PathVariable("value") String value) {
         return AESService.decryptFile(value, key);
     }
 
-    @RequestMapping("/des/enc/{key}/{value}")
+    @RequestMapping(value = "/des/enc/{key}/{value}", method = RequestMethod.POST)
     public String desEncryption(@PathVariable("key") String key, @PathVariable("value") String value) {
         return DESService.encryptFile(value, key);
     }
 
-    @RequestMapping("/des/dec/{key}/{value}")
+    @RequestMapping(value = "/des/dec/{key}/{value}", method = RequestMethod.POST)
     public String desDecryption(@PathVariable("key") String key, @PathVariable("value") String value) {
         return DESService.decryptFile(value, key);
+    }
+
+    @RequestMapping(value = "rsa/enc/{value}", method = RequestMethod.POST)
+    public RSAResult rsaEncryption(@PathVariable("value") String value){
+        RSAService rsa = new RSAService();
+        RSAResult result = new RSAResult();
+
+        result.setEncryptedString(rsa.encryptFile(value));
+        result.setPrivateKey(rsa.getPrivateKey());
+        result.setPublicKey(rsa.getPublicKey());
+
+        return result;
+    }
+
+    @RequestMapping(value = "rsa/dec", method = RequestMethod.POST)
+    public String rsaDecryption(@RequestBody RSAPayload payload){
+        RSAService rsa = new RSAService();
+        return rsa.decryptFile(payload.getValue(), payload.getKey());
     }
     
 }
