c594fd2beb1a1dc4d96ddbe078ae3c78d35fe4f1
Cooper F. Le <cooperfle@utdallas.edu>
2019-11-11 16:56:53 -0600

    feat: add aws payload

---
+++ src/main/java/com/cyberchase/cyberchaser/model/AWSPayload.java
@@ -0,0 +1,24 @@
+package com.cyberchase.cyberchaser.model;
+
+public class AWSPayload {
+    private String fileName;
+    private String contents;
+
+    public String getFileName() {
+        return this.fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getContents() {
+        return this.contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    
+}
