982febb382a1b7e5cb9efd666e823e07c12f0405
lukewilliamson98 <47017444+lukewilliamson98@users.noreply.github.com>
2019-11-11 18:39:55 -0600

    file upload

--- src/main/java/com/cyberchase/cyberchaser/Application.java
+++ src/main/java/com/cyberchase/cyberchaser/Application.java
@@ -8,11 +8,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean(name = "multipartResolver")
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(-1);
+        return multipartResolver;
     }
 
     @Bean
