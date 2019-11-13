6e9ca8dc2112ec9f7e19641d06da26dee407d78e
Cooper F. Le <cooperfle@utdallas.edu>
2019-11-13 16:48:04 -0600

    feat: added stuff whoops

--- src/main/java/com/cyberchase/cyberchaser/controller/EncryptionController.java
+++ src/main/java/com/cyberchase/cyberchaser/controller/EncryptionController.java
@@ -10,11 +10,8 @@ import com.cyberchase.cyberchaser.model.RSAPayload;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-import io.swagger.annotations.ApiParam;
 
 @RestController
 public class EncryptionController {
@@ -30,23 +27,6 @@ public class EncryptionController {
         return AESService.decryptString(value, key);
     }
 
-    @RequestMapping(value = "/aes/enc/file/{key}", method = RequestMethod.POST)
-    public String aesEncryptFile(
-            @ApiParam(name = "file", value = "Select the file to Upload", required = true)
-            @RequestPart("file") MultipartFile file,
-            @PathVariable("key") String key) {
-        return AESService.encryptFile(file, key);
-    }
-
-    @RequestMapping(value = "/aes/dec/file/{key}", method = RequestMethod.POST)
-    public String aesDecryptFile(
-        @ApiParam(name = "file", value = "Select the file to Upload", required = true)
-        @RequestPart("file") MultipartFile file,
-        @PathVariable("key") String key)
-    {
-        return AESService.decryptFile(file, key);
-    }
-
     @RequestMapping(value = "/des/enc/{key}/{value}", method = RequestMethod.POST)
     public String desEncryption(@PathVariable("key") String key, @PathVariable("value") String value) {
         return DESService.encryptString(value, key);
@@ -56,24 +36,6 @@ public class EncryptionController {
     public String desDecryption(@PathVariable("key") String key, @PathVariable("value") String value) {
         return DESService.decryptString(value, key);
     }
-
-    @RequestMapping(value = "/des/enc/file/{key}", method = RequestMethod.POST)
-    public String desEncryptFile(
-            @ApiParam(name = "file", value = "Select the file to Upload", required = true)
-            @RequestPart("file") MultipartFile file,
-            @PathVariable("key") String key) {
-        return DESService.encryptFile(file, key);
-    }
-
-    @RequestMapping(value = "/des/dec/file/{key}", method = RequestMethod.POST)
-    public String desDecryptFile(
-            @ApiParam(name = "file", value = "Select the file to Upload", required = true)
-            @RequestPart("file") MultipartFile file,
-            @PathVariable("key") String key)
-    {
-        return DESService.decryptFile(file, key);
-    }
-
 
     @RequestMapping(value = "rsa/enc/{value}", method = RequestMethod.POST)
     public RSAResult rsaEncryption(@PathVariable("value") String value){
--- src/main/java/com/cyberchase/cyberchaser/controller/HashController.java
+++ src/main/java/com/cyberchase/cyberchaser/controller/HashController.java
@@ -8,17 +8,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-import io.swagger.annotations.ApiParam;
 
 @RestController
 public class HashController {
 
     @RequestMapping(value = "/bcrypt/{value}", method = RequestMethod.POST)
     public ResponseEntity<String> bcryptHash(@PathVariable("value") String value) {
-        return ResponseEntity.ok(BcryptService.hash(value));
+        return ResponseEntity.ok(BcryptService.hash(value.getBytes()));
     }
 
     @RequestMapping(value = "/bcrypt/verify/{value}/{hash}", method = RequestMethod.POST)
@@ -34,13 +31,6 @@ public class HashController {
     @RequestMapping(value = "/md5/verify/{value}/{hash}", method = RequestMethod.POST)
     public boolean md5Check(@PathVariable("value") String value, @PathVariable("hash") String hash) {
         return MD5Service.checkHash(value, hash);
-    }
-
-    @RequestMapping(value = "/md5/file", method = RequestMethod.POST)
-    public String md5HashFile(
-            @ApiParam(name = "file", value = "Select the file to Upload", required = true)
-            @RequestPart("file") MultipartFile file) {
-        return MD5Service.hashFile(file);
     }
 
     @RequestMapping(value = "/Sha512/{hash}/{salt}", method = RequestMethod.POST)
